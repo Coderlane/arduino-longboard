4c391c401bc30d4b61150c681ae89b5c3c3bf430
Travis Lane <travis.lane.osx@gmail.com>
2015-03-04 13:54:33 -0800

    adding files

---
+++ Throttle.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file Throttle.cpp
+ * @brief 
+ * @author Travis Lane
+ * @version 0.1.0
+ * @date 2015-03-04
+ */
+
+#include "Throttle.h"
+
+/**
+ * @brief Creates a new throttle.
+ *
+ * @param new_pin The pin to read from.
+ * @param new_min (Optional) The minimum value. Defaults to 0.
+ * @param new_max (Optional) The maximum value. Defaults to 1023.
+ */
+Throttle::Throttle(int new_pin, int new_min, int new_max)
+{
+	pin = new_pin;
+
+	min = new_min;
+	max = new_max;
+	range = max - min;
+}
+
+/**
+ * @brief Read the percentage value from the pin.
+ *
+ * @return A percentage. 0 to 100 as an integer.
+ */
+int Throttle::Read()
+{
+	int val = analogRead(pin);
+
+	if(val <= min) {
+		// Low range;
+		return 0;
+	}
+
+	if(val >= max) {
+		// High range
+		return 100;
+	}
+
+	val -= new_min; // Shift downward
+	return val / range; // Compute result
+}
---
+++ Throttle.h
@@ -0,0 +1,30 @@
+/**
+ * @file Throttle.h
+ * @brief 
+ * @author Travis Lane
+ * @version 0.1.0
+ * @date 2015-03-04
+ */
+
+#ifndef THROTTLE_H
+#define THROTTLE_H
+
+#define THROTTLE_DEFAULT_MIN 0
+#define THROTTLE_DEFAULT_MAX 1023
+
+class Throttle
+{
+private:
+  int pin;
+	int min;
+  int max;
+	int range;
+public:
+  Throttle(int new_pin,
+           int new_min = THROTTLE_DEFAULT_MIN,
+           int new_max = THROTTLE_DEFAULT_MAX);
+
+  int Read();
+}
+
+#endif /* THROTTLE_H */
